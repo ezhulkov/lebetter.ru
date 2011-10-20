006f772de54fcb65987d0694ce52bfe9e961c45b
Eugene Zhulkov <ezhulkov@gmail.com>
2011-10-20 13:26:30 +0400

    загрузка логотипа

---
+++ lebetter-portal/src/main/java/org/ohm/lebetter/tapestry5/web/components/base/AbstractPhotoComponent.java
@@ -0,0 +1,28 @@
+package org.ohm.lebetter.tapestry5.web.components.base;
+
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.room13.mallcore.model.ImageAware;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ezhulkov
+ * Date: 21.04.2009
+ * Time: 17:01:24
+ * To change this template use File | Settings | File Templates.
+ */
+@Import(
+        stylesheet = {"proxy:/redactor/css/redactor.css"},
+        library = {"proxy:/redactor/redactor.js"}
+)
+public abstract class AbstractPhotoComponent<T extends ImageAware>
+        extends AbstractBaseComponent {
+
+    @Parameter(name = "object", required = false, allowNull = false)
+    private T selectedObject;
+
+    public T getSelectedObject() {
+        return selectedObject;
+    }
+
+}
